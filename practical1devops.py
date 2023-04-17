d4b6b9ba350979f0111e8b98cfffd094567060e4
Vikrant <vikrantsk.22@ichat.sp.edu.sg>
2023-04-17 09:50:12 +0800

    newest vn of prac1devops

--- practical1devops.py
+++ practical1devops.py
@@ -6,3 +6,14 @@ It is necessary to have git add to add files to the staging area before committi
 
 It is to describe the version or to explain why the commit was necessary. Commits are a checkpoint that describes the status of the project in the timeline.
 
+3. What does the green and red color coded line mean when you issue git diff?
+
+It shows the differences between the past and present version of the file.
+
+4. Under what circumstances will you use branching in a software development project?
+
+To maintain stability of the file while isolated changes are made in the file, like bug fixes.
+
+4.8 git status, 
+
+
