3abc5914f7e404edf678094dd5f53da5d01df59e
Vikrant <vikrantsk.22@ichat.sp.edu.sg>
2023-04-17 09:30:46 +0800

    answers to devops lab1

---
+++ practical1devops.py
@@ -0,0 +1,8 @@
+1. Why is it necessary to have git add in the workflow?
+
+It is necessary to have git add to add files to the staging area before committing them.
+
+2. What is the purpose of describing changes in the commit command?
+
+It is to describe the version or to explain why the commit was necessary. Commits are a checkpoint that describes the status of the project in the timeline.
+
